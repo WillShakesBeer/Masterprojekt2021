6284f997f6a075bccc99230f17b0faf67cb6f2c7
WillShakesBeer <zwergenkoenig@gmx.net>
2021-11-23 16:22:54 +0100

    Final Fix

---
+++ src/RunDis.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Martin Eberle aka WillShakesBeer on 23.11.2021.
+ */
+public class RunDis {
+    public static void main (String[] args){
+        System.out.println("Hello Cruel World");
+    }
+}

375c47dbe1956b5fa471d85b2ab4fdff7defd065
WillShakesBeer <zwergenkoenig@gmx.net>
2021-11-23 18:43:37 +0100

    final fix

---
+++ src/Data/VictoryPoint.java
@@ -0,0 +1,14 @@
+package Data;
+
+/**
+ * Created by Martin Eberle aka WillShakesBeer on 23.11.2021.
+ */
+public class VictoryPoint {
+    Data.Coord coord;
+
+    public VictoryPoint(Data.Coord coord , ) {
+        this.coord=coord;
+
+    }
+
+}
